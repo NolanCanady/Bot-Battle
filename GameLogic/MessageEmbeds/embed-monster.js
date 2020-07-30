5e027e978cd252a8285904f63b9a917955270c9b
NolanCanady <nolancanady547@gmail.com>
2020-07-30 09:34:38 -0500

    add custom message embeds
    changed to response of the bot to be an embed reply, allowing for easier organization of the datat

---
+++ GameLogic/MessageEmbeds/embed-monster.js
@@ -0,0 +1,21 @@
+
+
+
+function monster(creature){
+  if(creature != null){
+    const msg = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle(creature.name)
+      .setImage(creature.image)
+      .addFields(
+        { name: 'hp', value: creature.stats.hp, inline: true},
+        { name: 'att', value: creature.stats.att, inline: true},
+        { name: 'def', value: creature.stats.def, inline: true},
+        { name: 'spd', value: creature.stats.spd, inline: true}
+      )
+      .setFooter('Available actions: !attack - !shield - !item - !run');
+    return msg;
+  }else{
+    return;
+  }
+};
--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ var logger = require('winston');
 var auth = require('./auth.json');
 var creatureLoad = require('./GameLogic/Creatures.json');
 var itemlistLoad = require('./GameLogic/ItemList.json');
+var masterEmbed = require('./GameLogic/MessageEmbeds/masterembed.js');
 var fs = require('fs');
 
 const bot = new Discord.Client();
@@ -55,8 +56,8 @@ bot.on('message', msg => {
           bag:{}
         },
         activity: {
-          "current": "nothing",
-          "last": "nothing"
+          "current": "",
+          "last": ""
         }
       }
       var data = JSON.stringify(newPlayer);
@@ -64,9 +65,9 @@ bot.on('message', msg => {
       msg.reply(' welcome, your new character, '+args[0]+', has awaken');
     }
 
-    if(cmd == "myLevel"){
+    if(cmd == "characterInfo"){
       var playerFile = fs.readFileSync('Players/'+msg.author.id+".json"); //replace with try
-      msg.reply(' you are level: '+JSON.parse(playerFile).stats.level);
+      msg.reply(self(msg.author.id));
     }
 
     if(cmd == "encounter"){
@@ -74,7 +75,7 @@ bot.on('message', msg => {
       //make sure the player exists, if not message them to create a newcharacter
       var player = JSON.parse(playerFile);
 
-      if(player.activity.current == "nothing"){
+      if(player.activity.current == ""){
         var difficulty = 1; //replace with random later
         var randomCreature = 0; //replace with random later
         var finalCreature = creatureLoad[difficulty][randomCreature];
@@ -89,50 +90,59 @@ bot.on('message', msg => {
         player = JSON.stringify(player);
         fs.writeFileSync('Players/'+msg.author.id+".json", player);
         //link images and make the response message fancier
-        msg.reply("You have enountered a "+finalCreature.name+"... prepare for battle! (!attack, !eat, !shield, !cancel)") //update this later to be a randomly chosen phrase
+        //msg.reply("You have enountered a "+finalCreature.name+"... prepare for battle! (!attack, !eat, !shield, !cancel)") //update this later to be a randomly chosen phrase
+        msg.reply("Prepare for your encounter...");
+        msg.reply(monster(finalCreature));
       }else{
-        msg.reply("it seems you are too preoccupied with: "+player.activity+" to start an encounter right now... use !cancel to cancel your current action.");
+        msg.reply("it seems you are too preoccupied with: "+player.activity.current+" to start an encounter right now... use !cancel to cancel your current action.");
       }
     }
 
     if(cmd == "cancel"){
       var playerFile = fs.readFileSync('Players/'+msg.author.id+".json");
       var player = JSON.parse(playerFile);
-      player.activty = {current: "nothing", "last": player.activity};
-      player = JSON.stringify(player);
-      fs.writeFileSync('Players/'+msg.author.id+".json", player);
+      player.activity.last = player.activity.current;
+      player.activity.current = "";
+      fs.writeFileSync('Players/'+msg.author.id+".json",JSON.stringify(player));
       msg.reply("you have cancel your actions.");
     }
   }
-  /*  if(msg.content === '!join'){
-    var newPlayer = {
-      id: msg.author.id,
-      name: msg.author.user,
-      level: '1',
-      stats:{
-        hp: '10',
-        att: '2',
-        spd: '3',
-        def: '5'
-      },
-      inventory:{
-        equipment:{
-          helmet: '',
-          chest: '',
-          legs: '',
-          hands: '',
-          right: 'fist',
-          left: 'fist',
-        },
-        bag:{}
-      }
-    }
-    let data = JSON.stringify(newPlayer)
-    msg.reply(' welcome'+msg.author.id);
-  }
-  if(msg.content === '!level'){
-    fs.readFile('Players/'+msg.author.id+".txt", function(err, data){
-
-    });
-  }*/
 })
+
+const monster = function(creature){
+  if(creature != null){
+    const msg = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle(creature.name)
+      .setImage(creature.image)
+      .addFields(
+        { name: 'hp', value: creature.stats.hp, inline: true},
+        { name: 'att', value: creature.stats.att, inline: true},
+        { name: 'def', value: creature.stats.def, inline: true},
+        { name: 'spd', value: creature.stats.spd, inline: true}
+      )
+      .setFooter('Available actions: !attack - !shield - !item - !run');
+    return msg;
+  }else{
+    return;
+  }
+}
+
+const self = function(id){
+  var player = fs.readFileSync('Players/'+id+".json");
+  player = JSON.parse(player);
+
+  const msg = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle(player.name)
+    .addFields(
+      { name: 'hp', value: player.stats.hp, inline: true},
+      { name: 'xp', value: player.stats.xp, inline:true},
+      { name: '\u200B', value: '\u200B'},
+      { name: 'att', value: player.stats.att, inline: true},
+      { name: 'def', value: player.stats.def, inline: true},
+      { name: 'spd', value: player.stats.spd, inline: true},
+    )
+    .setFooter('Status: stable - Actions: !inventory !rest !heal !item');
+    return msg;
+}
